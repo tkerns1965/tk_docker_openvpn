daf56f807ec752574aacbe144044e65599db079d
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-22 11:56:43 -0400

    Update process_client_csr.sh

--- process_client_csr.sh
+++ process_client_csr.sh
@@ -2,3 +2,6 @@
 
 docker cp cnt_tkdovpn_client:/etc/openvpn/easy-rsa/keys/client1.csr .
 docker cp client1.csr cnt_tkdovpn_server:/etc/openvpn/easy-rsa/keys/
+docker exec cnt_tkdovpn_server bash "cd /etc/openvpn/easy-rsa/ \
+        && source ./vars \
+        && ./sign-req --batch client1"

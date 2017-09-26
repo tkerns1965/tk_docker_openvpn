11cf959dd1893d3e1237d94e7dd74eb8ff139fe5
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-26 14:40:52 -0400

    Update start_tkdovpn.sh

--- start_tkdovpn.sh
+++ start_tkdovpn.sh
@@ -7,7 +7,6 @@ if [ -z "$CLIENT_NAME" ]; then
   exit 1
 fi
 
-docker run --name cnt_tkdovpn_server1 -d -p 1194:1194/udp --device=/dev/net/tun img_tkdovpn_server1
-# docker run --name cnt_tkdovpn_server1 -d -p 1194:1194/udp --cap-add=NET_ADMIN --device=/dev/net/tun img_tkdovpn_server1
+docker run --name cnt_tkdovpn_server1 -d -p 1194:1194/udp --cap-add=NET_ADMIN --device=/dev/net/tun img_tkdovpn_server1
 # docker run --name cnt_tkdovpn_server1 -d -p 1194:1194/udp --privileged img_tkdovpn_server1
 # docker run img_tkdovpn_$CLIENT_NAME

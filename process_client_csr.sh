f1c0bc05294459b8ae190646df5e8f3cf7d0bf67
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-26 13:59:31 -0400

    Update process_client_csr.sh

--- process_client_csr.sh
+++ process_client_csr.sh
@@ -7,15 +7,20 @@ if [ -z "$CLIENT_NAME" ]; then
   exit 1
 fi
 
-docker run --name cnt_tkdovpn_client -v vol_tkdovpn_certs:/tkdovpn/certs \
+docker run --name cnt_tkdovpn_client_tmp -v vol_tkdovpn_certs:/tkdovpn/certs \
   img_tkdovpn_client ./build_client_csr.sh $CLIENT_NAME
-docker commit cnt_tkdovpn_client img_tkdovpn_client_a
-docker rm cnt_tkdovpn_client
-docker run --name cnt_tkdovpn_server -v vol_tkdovpn_certs:/tkdovpn/certs \
+docker commit cnt_tkdovpn_client_tmp img_tkdovpn_client_tmp
+docker rm cnt_tkdovpn_client_tmp
+
+docker run --name cnt_tkdovpn_server_tmp -v vol_tkdovpn_certs:/tkdovpn/certs \
   img_tkdovpn_server ./sign_client_csr.sh $CLIENT_NAME
-docker commit cnt_tkdovpn_server img_tkdovpn_server1
-docker rm cnt_tkdovpn_server
-docker run --name cnt_tkdovpn_client_a -v vol_tkdovpn_certs:/tkdovpn/certs \
-  img_tkdovpn_client_a ./install_client_crt.sh $CLIENT_NAME
-docker commit cnt_tkdovpn_client_a img_tkdovpn_$CLIENT_NAME
-docker rm cnt_tkdovpn_client_a
+docker commit -c "ENTRYPOINT ["openvpn", "--cd", "/etc/openvpn/", "--config", "server.conf"]" \
+  -c "EXPOSE 1194/udp" cnt_tkdovpn_server_tmp img_tkdovpn_server1
+docker rm cnt_tkdovpn_server_tmp
+
+docker run --name cnt_tkdovpn_client_tmp -v vol_tkdovpn_certs:/tkdovpn/certs \
+  img_tkdovpn_client_tmp ./install_client_crt.sh $CLIENT_NAME
+docker commit -c "ENTRYPOINT ["openvpn", "--cd", "/etc/openvpn/", "--config", "client.conf"]" \
+  cnt_tkdovpn_client_tmp img_tkdovpn_$CLIENT_NAME
+docker rm cnt_tkdovpn_client_tmp
+docker rmi img_tkdovpn_client_tmp

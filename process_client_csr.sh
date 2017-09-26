17bd1247abc0d281925d8eb30f1418cdd01031ad
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-26 04:34:17 -0400

    Update process_client_csr.sh

--- process_client_csr.sh
+++ process_client_csr.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm img_tkdovpn_client ./build_client_csr.sh
+CLIENT_NAME=$1
+
+if [ -z "$CLIENT_NAME" ]; then
+  echo "No client name specified."
+  exit 1
+fi
+
+docker run --rm img_tkdovpn_client ./build_client_csr.sh $CLIENT_NAME

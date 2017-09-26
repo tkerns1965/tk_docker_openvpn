8cfd04421c7e565ae3b3a8ffcd66e1a0a32fa897
tkerns1965 <tony.kerns.65@gmail.com>
2017-09-26 10:49:25 -0400

    Update process_client_csr.sh

--- process_client_csr.sh
+++ process_client_csr.sh
@@ -9,3 +9,5 @@ fi
 
 docker run -v vol_tkdovpn_certs:/tkdovpn/certs --rm img_tkdovpn_client ./build_client_csr.sh $CLIENT_NAME
 docker run -v vol_tkdovpn_certs:/tkdovpn/certs --rm img_tkdovpn_server ./sign_client_csr.sh $CLIENT_NAME
+docker run -v vol_tkdovpn_certs:/tkdovpn/certs img_tkdovpn_client ./install_client_crt.sh $CLIENT_NAME
+docker commit img_tkdovpn_client img_tkdovpn_$CLIENT_NAME
